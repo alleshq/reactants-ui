1543b11974d0023478688afd2bf2ffc2706f6268
Dante <dante@issaias.net>
2020-04-30 03:26:23 +0100

    Card header

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -3,10 +3,12 @@ import useTheme from "../styles/use-theme";
 import withDefaults from "../utils/with-defaults";
 
 interface Props {
+  header?: string;
   className?: string;
 }
 
 const defaultProps = {
+  header: "",
   className: "",
 };
 
@@ -15,6 +17,7 @@ export type CardProps = Props & typeof defaultProps & NativeAttrs;
 
 const Card: React.FC<React.PropsWithChildren<CardProps>> = ({
   children,
+  header,
   className,
   ...props
 }) => {
@@ -23,11 +26,25 @@ const Card: React.FC<React.PropsWithChildren<CardProps>> = ({
 
   return (
     <div ref={cardRef} className={`card ${className}`} {...props}>
-      {children}{" "}
+      {header && <div className="header">{header}</div>}
+      <div className="content">{children}</div>
       <style jsx>{`
         .card {
           border: 1px solid ${theme.palette.foreground}22;
           border-radius: 5px;
+        }
+
+        .header {
+          background: ${theme.palette.foreground}05;
+          border-bottom: 1px solid ${theme.palette.foreground}22;
+          color: ${theme.palette.foreground}bb;
+          width: calc(100% - 30px);
+          padding: 10px 15px;
+        }
+
+        .content {
+          padding: 25px;
+          width: calc(100% - 50px);
         }
       `}</style>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,54 +8,46 @@ export default () => {
     <main>
       <h3>Buttons</h3>
 
-      <h4>Colors</h4>
-      <Card>
+      <Card header="Colors">
         <Button>Default</Button>
         <Button type="primary">Primary</Button>
         <Button type="danger">Danger</Button>
       </Card>
 
-      <h4>States</h4>
-
-      <Card>
+      <Card header="States">
         <Button disabled>Disabled</Button>
       </Card>
 
-      <h4>Sizes</h4>
-      <Card>
+      <Card header="Sizes">
         <Button size="small">Small</Button>
         <Button size="medium">Medium</Button>
         <Button size="large">Large</Button>
       </Card>
 
-      <h4>Custom Width</h4>
-      <Card>
+      <Card header="Custom Width">
         <Button width={100}>Width: 100px</Button>
         <Button width="50%">Width: 50%</Button>
         <Button width="100%">Width: 100%</Button>
       </Card>
 
-      <h4>Icon</h4>
-      <Card>
-        <Button icon={<Trash2 />} size="large">
+      <Card header="Icon">
+        <Button type="primary" icon={<Trash2 />} size="large">
           Delete
         </Button>
 
-        <Button icon={<Trash2 />} size="large" iconSide="right">
+        <Button type="primary" icon={<Trash2 />} size="large" iconSide="right">
           Delete
         </Button>
       </Card>
 
       <h3>Inputs</h3>
 
-      <h4>Colors</h4>
-      <Card>
+      <Card header="Colors">
         <Input placeholder="Default" />
         <Input type="danger" placeholder="Danger" />
       </Card>
 
-      <h4>Custom Width</h4>
-      <Card>
+      <Card header="Custom Width">
         <Input placeholder="Width: 100px" width={100} />
         <Input placeholder="Width: 50%" width="50%" />
         <Input placeholder="Width: 100%" width="100%" />
@@ -63,24 +55,21 @@ export default () => {
 
       <style jsx>{`
         h3 {
-          margin: 15px 0 30px;
-        }
-
-        h4 {
-          margin: 20px 0 5px;
-          font-weight: 500;
+          margin: 45px 0 25px;
         }
 
         main {
-          padding: 25px 50px 50px;
+          padding: 0 10px 50px;
           max-width: 800px;
           margin: 0 auto;
         }
 
         :global(.card) {
-          padding: 40px;
           margin: 10px 0;
           width: 100%;
+        }
+
+        :global(.card) > :global(.content) {
           display: inline-grid;
           gap: 15px;
           justify-items: start;
