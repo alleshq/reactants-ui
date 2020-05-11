a19ab35f7b29f39e719af29d4d1de7f30bd79a69
Dante <dante@issaias.net>
2020-05-11 17:48:29 +0100

    Large cleanup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "@rollup/plugin-node-resolve";
+import postcss from "rollup-plugin-postcss";
+import pkg from "./package.json";
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: "esm",
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    external(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({ extract: true }),
+  ],
+};
--- src/col/col.tsx
+++ src/col/col.tsx
@@ -1,4 +1,4 @@
-import { memo } from "react";
+import React, { memo } from "react";
 import styles from "./col.module.css";
 
 interface Props extends React.HTMLAttributes<any> {
